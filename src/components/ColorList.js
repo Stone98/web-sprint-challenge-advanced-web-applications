4eb80bf98d93390182a6b98277268417ad6944bf
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-02-26 04:40:02 -0700

    id

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -50,7 +50,7 @@ const ColorList = ({ colors, updateColors }) => {
       <p>colors</p>
       <ul>
         {colors.map(color => (
-          <li key={color.color} onClick={() => editColor(color)}>
+          <li key={color.color} data-testid="/color/i" onClick={() => editColor(color)}>
             <span>
               <span className="delete" onClick={e => {
                 e.stopPropagation();
