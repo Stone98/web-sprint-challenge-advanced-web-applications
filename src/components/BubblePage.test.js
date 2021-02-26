cd5bdc9ede7ad6740ff1976a0001f4c2ef39b7fe
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-02-26 05:49:16 -0700

    added data

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,6 +1,21 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import BubblePage from './BubblePage';
+import React from 'react'
+import { axiosWithAuth } from '../helpers/axiosWithAuth';
+import { render, screen } from '@testing-library/react'
+import BubblePage from './BubblePage'
+
+const colorsData = [
+  { color: 'aliceblue', code: { hex: '#f0f8ff' }, id: 1 },
+  { color: 'limegreen', code: { hex: '#99ddbc' }, id: 2 },
+  { color: 'aqua', code: { hex: '#00ffff' }, id: 3 },
+  { color: 'aquamarine', code: { hex: '#7fffd4' }, id: 4 },
+  { color: 'lilac', code: { hex: '#9a99dd' }, id: 5 },
+  { color: 'softpink', code: { hex: '#dd99ba' }, id: 6 },
+  { color: 'bisque', code: { hex: '#dd9a99' }, id: 7 },
+  { color: 'softyellow', code: { hex: '#dcdd99' }, id: 8 },
+  { color: 'blanchedalmond', code: { hex: '#ffebcd' }, id: 9 },
+  { color: 'blue', code: { hex: '#6093ca' }, id: 10 },
+  { color: 'blueviolet', code: { hex: '#8a2be2' }, id: 11 },
+]
 
 test('Renders BubblePage without errors', () => {
   // Finish this test
@@ -9,7 +24,7 @@ test('Renders BubblePage without errors', () => {
 
 test('Fetches data and renders the bubbles on mounting', () => {
   // Finish this test
-  render(<BubblePage />);
+  render(<BubblePage />)
 })
 
 //Task List
