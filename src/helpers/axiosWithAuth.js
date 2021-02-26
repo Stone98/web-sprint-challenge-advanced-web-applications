4394f68d79df9205b2fd327fd3ea35c96706b7ba
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-02-26 03:41:59 -0700

    axiosWithAuth created

--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -2,3 +2,9 @@ import axios from "axios";
 
 //Task List:
 //Build and export a function used to send in our authorization token
+export const axiosWithAuth = () => {
+    return axios
+        .create({
+            headers: { authorization: localStorage.getItem("token") },
+        })
+}
